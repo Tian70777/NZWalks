a96535ba8cf0d796d9fcb0dcef60fc38eae83d97
Tian70777 <tian0247@zbc.dk>
2024-07-01 15:05:43 +0200

    20240701

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -1,42 +1,62 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
 using NZWalks.API.Models.Domain;
 using static System.Net.WebRequestMethods;
 
 namespace NZWalks.API.Controllers
 {
+    // use DbContext to talk to the dband get the result back
+    // have inject dbcontext in program.cs
+    // now can use a constructor to inject the dbcontext
+    
     // https://localhost:1234/api/regions
-    // poing to the regions controller
+    // pointing to the regions controller
     [Route("api/[controller]")]
     [ApiController]
 
     // CRUD operations on this region's resource
     public class RegionsController : ControllerBase
     {
-        // action method, [attribute]
+        private readonly NZWalksDbContext dbContext;
+
+        // use DbContext to talk to the dband get the result back
+        // have inject dbcontext in program.cs
+        // now can use a constructor to inject the dbcontext
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            // assign the provate foeld here, so can use dbcontext in the methods
+            this.dbContext = dbContext;
+        }
+
+        // GET ALL REGIONS
+        // GET: https//localhost:1234/api/regions
         [HttpGet]
         public IActionResult GetAll()
         {
-            var regions = new List<Region>
-            {
-                
-                
-                
-                
-                new Region{
-                    Id = Guid.NewGuid(),
-                    Name = "Auckland",
-                    Code = "AKL",
-                    RegionImageUrl = "https://i0.wp.com/www.yolosolotravel.com/wp-content/uploads/2021/11/auckland-map.jpg?resize=800%2C774&ssl=1"
-                },
-                new Region{
-                    Id = Guid.NewGuid(),
-                    Name = "Wellington Region",
-                    Code = "WLG",
-                    RegionImageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2fy0QYm9K1G__E_NtqaQcUc65Mma9iBZ5aQ&s"
-                }
-            };
+            var regions = dbContext.Regions.ToList();
+
             return Ok(regions);
         }
+
+        // GET region by Id
+        // GET: https//localhost:1234/api/regions/{id}
+        [HttpGet]
+        //add a route attribute 
+        [Route("{id:Guid}")]
+        public  IActionResult GetById([FromRoute] Guid id)
+        {
+            // Find method only takes the Primary Key
+            // var region = dbContext.Regions.Find(id);
+
+            var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+            if( region == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(region);
+        }
+
     }
 }
